922869e40d7e083e071adc286ac36bb204f76011
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-15 19:52:10 -0300

    very bad solution

--- course/src/model/entities/Reservation.java
+++ course/src/model/entities/Reservation.java
@@ -34,7 +34,7 @@ public class Reservation {
 	}
 	
 	public long duration() {
-		long diff = checkIn.getTime() - checkOut.getTime();
+		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
@@ -45,7 +45,7 @@ public class Reservation {
 	
 	@Override
 	public String toString() {
-		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ",  check-out" + sdf.format(checkOut) + ", " + duration() + " nights";
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ",  check-out " + sdf.format(checkOut) + ", " + duration() + " nights";
 	}
 	
 }
